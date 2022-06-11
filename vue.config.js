028952eac50eb9589dbbd83614abb4ed29a6a736
oehlers <samuel.oehler@bluewin.ch>
2022-06-11 16:29:05 +0200

    1629

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,7 @@
 module.exports = {
     pages: {
         'index': {
-            //entry: './01a_Basics/01_Intro/src/main.js',
-            entry: './01c_Basics/01_Intro/src/main.js',
-            //entry: './02b_Watchers/00_Demo/src/main.js',
-            //entry: './03a_Routing/00_Demo/src/main.js',
-            //entry: './03a_Routing/01_Intro/src/main.js',
-            //entry: './04a_Routing-Advanced/01_Intro/src/main.js',
+            entry: './01_intranet/src/main.js'
             template: 'public/index.html'
         }
     }
